16c81799356a6da17cd53ffd0d50549de4308a75
TbotInnovations <149735728+JoyzaTech@users.noreply.github.com>
2024-12-12 14:39:22 -0700

    v0.1
    - Added logo
    - Setup react project

---
+++ src/components/logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/logo.css';
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="logo">
+        &lt;&gt;<br></br> Cipher <br /> Stacker <br />&lt;/&gt;
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
